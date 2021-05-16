d5e7b92557a45084c1dded34b442b706115f1fb4
ak3zaidan123 <84095687+ak3zaidan123@users.noreply.github.com>
2021-05-16 15:37:02 -0700

    Add files via upload

---
+++ assignment6a.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class assignment5 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class assignment6a
+{
+   public class Coin { 
+ 
+private double myValue; 
+private String myName; 
+ 
+public Coin(double value, String name) { 
+myValue = value; 
+myName = name; 
+} 
+ 
+public double getValue() { 
+return myValue; 
+} 
+ 
+public String getName() { 
+return myName;
+} 
+ if(aCoin.getValue() == myValue) && (aCoin.getName() == myName)
+ return true;
+ else 
+ return false;
+ }
+}
+
