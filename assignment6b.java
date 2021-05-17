15816c0735bc31885488791fc2f9d39ebe9940ae
ak3zaidan123 <84095687+ak3zaidan123@users.noreply.github.com>
2021-05-16 17:04:50 -0700

    Add files via upload

---
+++ assignment6b.java
@@ -0,0 +1,50 @@
+;
+/**
+ * Write a description of class assignment6 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class assignment6b
+{
+    public class Purse { 
+private ArrayList coins; 
+ 
+public Purse() { 
+coins = new ArrayList(); 
+}
+  
+public void add(Coin aCoin) { 
+coins.add(aCoin); 
+} 
+ 
+public double getTotal() { 
+	double total;
+	for(int i=0; i<coins.length; i++){
+	total += coins;
+	return total;
+} 
+}
+public int count(Coin aCoin) {
+   int x = 0;
+   for(int i=0; i<= coins.length; i++){
+       if(aCoin == coins());
+       x+=1;
+       return x;
+ } 
+ 
+}
+public int find(Coin aCoin) {
+    for(int i=0; i<= coins.length; i++){
+        if(aCoin == .25 && aCoin < coins())
+        return myName;
+        if(aCoin == .1 && aCoin < coins())
+        return myName;
+        if(aCoin == .05 && aCoin < coins())
+        return myName;
+        if(aCoin == .01 && aCoin < coins())
+        return myName;
+}
+}
+}
+
