d3dc80eeffc6cf80f7395415089b79ccf4cb31aa
ak3zaidan123 <84095687+ak3zaidan123@users.noreply.github.com>
2021-05-18 20:20:57 -0700

    Add files via upload

---
+++ vehicle.java
@@ -0,0 +1,28 @@
+
+/**
+ * 
+ *
+ * Ahmed
+ * may 17
+ */
+public class vehicle
+{
+    String brand = "Ford";
+    String color = "black";
+   
+    public void honk() {                    
+    System.out.println("Tuut, tuut");
+    }
+    
+    public void pulledover(){
+        System.out.println("vin num: B1Y78263927: insurance card: id");
+    }
+    
+    public String getColor(){
+        return "yellow";
+    }
+     int averagerimsize = 19;
+    public int getintaveragerimsize(){
+        return averagerimsize;
+    }
+}
